8c72807e3ce1744b5e2f3fd5d9dab07fc9152ebe
Paseleon <76861131+Yubikitasu@users.noreply.github.com>
2025-08-06 16:52:23 +0700

    Final touches

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-const video = document.querySelector("video");
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import {
   getFirestore,
@@ -9,18 +7,22 @@ import {
   doc,
   onSnapshot,
   addDoc,
-  deleteDoc,
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-import firebaseConfig from "./firebase-config.js"
 
-// Change your firebase config
-// Create your own Firebase project, set up your own Firestore, then config it here.
+// -------------------------------------------
+// IMPORTANT: Create your own Firebase project, 
+// then configure it in src/firebase-config.js.
+// -------------------------------------------
+import firebaseConfig from "./firebase-config.js"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+
+// STUN server configuration
+// This is used to establish a WebRTC connection.
 const servers = {
   iceServers: [
     {
@@ -30,29 +32,33 @@ const servers = {
   iceCandidatePoolSize: 10,
 };
 
-// Global states
-
+// Create a new RTCPeerConnection
 let pc = new RTCPeerConnection(servers);
 
+// Getting the video stream from the user's device as a MediaStream
 let remoteStream = new MediaStream();
 
+// Get the remote video stream from the RTCPeerConnection, created from the application.
 pc.ontrack = (e) => {
   e.streams[0].getTracks().forEach((track) => {
     remoteStream.addTrack(track);
-    // console.log("Got track: ", track);
   });
 };
+
+// Then set the source of the video element to the remote stream,
+// so that the screen (video and audio) can be displayed in the browser.
+document.getElementById("remote").srcObject = remoteStream;
 
 const videoBox = document.getElementById("remote");
 
 let remoteChannel;
 
 pc.ondatachannel = (event) => {
+  // Getting the data channel from the event
   remoteChannel = event.channel;
 
   remoteChannel.onopen = () => {
-    let timeoutId;
-
+    // When the data channel is open, we can start sending mouse and keyboard events
     videoBox.addEventListener("mousemove", (event) => {
       const rect = videoBox.getBoundingClientRect(); // Get element's position on the page
       const x = event.clientX - rect.left; // Mouse X relative to element
@@ -62,6 +68,8 @@ pc.ondatachannel = (event) => {
       const virtualX = (x / rect.width) * 1920;
       const virtualY = (y / rect.height) * 1080;
 
+      // Send mouse move event to the peer connection, making the remote desktop 
+      // responsive to mouse movements.
       remoteChannel.send(
         JSON.stringify({
           x: virtualX.toFixed(0),
@@ -71,6 +79,7 @@ pc.ondatachannel = (event) => {
       );
     });
 
+    // Listening for mouse and keyboard events and sending them to the remote peer
     videoBox.addEventListener("mousedown", (event) => {
       let button;
       if (event.button === 0) {
@@ -86,6 +95,8 @@ pc.ondatachannel = (event) => {
       );
     });
 
+    // Handling mouse up events to stop the mouse click
+    // not making the mouse to hold its click.
     videoBox.addEventListener("mouseup", (event) => {
       let button;
       if (event.button === 0) {
@@ -99,64 +110,89 @@ pc.ondatachannel = (event) => {
           button: button
         })
       );
-    })
+    });
+
+    // Sending keyboard events to the remote peer
+    // This allows the user to type on the remote desktop.
+    document.addEventListener("keydown", (event) => {
+      remoteChannel.send(
+        JSON.stringify({
+          type: "keydown",
+          key: event.key,
+        })
+      );
+    });
+
+    document.addEventListener("keyup", (event) => {
+      remoteChannel.send(
+        JSON.stringify({
+          type: "keyup",
+          key: event.key.toUpperCase(),
+        })
+      );
+    });
   };
 };
 
+// Preventing right-click context menu on the video element
+// This is to ensure that when right-clicking on the video, the context menu does not appear,
+// making it easier to interact with the remote desktop.
 videoBox.addEventListener("contextmenu", (event) => {
   event.preventDefault();
 });
-
-document.getElementById("remote").srcObject = remoteStream;
 
 const answerButton = document.getElementById("answerButton");
 
 answerButton.addEventListener("click", async (e) => {
   e.preventDefault();
 
+  // Getting the ID from the input field,
+  // connecting to the RTCPeerConnection
+  // and setting up the answer to the call.
   const answerId = document.getElementById("answerId").value;
   const callDocRef = doc(db, "calls", answerId);
   const callDoc = await getDoc(callDocRef);
   const offer = callDoc.data();
 
+  // Creating offer and answer ICE candidates, establishing the connection
+  // and setting up the data channel for communication.
   const answerCandidateRef = await collection(callDocRef, "answerCandidates");
   const offerCandidateRef = await collection(callDocRef, "offerCandidates");
 
+  // Creating ICE candidates for the answer, which the application will
+  // add to its ICE candidates.
   pc.onicecandidate = (event) => {
     if (event.candidate) {
       addDoc(answerCandidateRef, event.candidate.toJSON());
-      // .then(console.log("Added answer candidate."))
-      // .catch(e => {console.log("Something happended! Error: ", e)});
-      // console.log(event.candidate.toJSON());
     }
   };
 
+  // Setting the remote description with the offer received from the application.
   await pc.setRemoteDescription(offer);
+
+  // Creating an answer to the offer and setting it as the local description.
   const answer = await pc.createAnswer();
   await pc
     .setLocalDescription(answer)
     .then(async () => {
       await setDoc(callDocRef, answer);
-      console.log("Answered call");
     })
     .catch((e) => console.log(e));
-  // console.log(callDoc.data());
 
+  // Listening for ICE candidates from the offer and adding them to our ICE candidates.
   const snapshotRemoteCandidate = onSnapshot(offerCandidateRef, (snapshot) => {
     snapshot.docChanges().forEach((change) => {
       if (change.type === "added") {
         pc.addIceCandidate(change.doc.data());
-        // console.log(change.doc.data());
       }
     });
   });
-
-  const videoBox = document.getElementById("remote");
-  const cursorRef = await doc(collection(callDocRef, "mouse"), "mousePosition");
 });
 
 const callDocsRef = collection(db, "calls");
 
+// Listen for calls in the "calls" collection
+// and display the IDs of new offers in the "callerIds" div.
 const getIds = onSnapshot(callDocsRef, (snapshot) => {
   snapshot.docChanges().forEach((change) => {
     if (change.type === "added") {
