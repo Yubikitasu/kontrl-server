b2ddcd3fbd5a18b4a4898367e4f0a0707a2eb2e4
Paseleon <76861131+Yubikitasu@users.noreply.github.com>
2025-08-07 11:25:15 +0700

    .

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,16 @@
+// -------------------------------------------
+// IMPORTANT: Create your own Firebase project, 
+// add the configuration to your .env.local file,
+// and then import it in src/firebase-config.js.
+// -------------------------------------------
 const firebaseConfig = {
-  // Enter your Firebase config from the Firebase website here.
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 
 export default firebaseConfig;
